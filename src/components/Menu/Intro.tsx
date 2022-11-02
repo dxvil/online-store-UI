56a2267ec5c222b5d5f35007d06ead37a0298b9a
vol4ik europe <volikeurope@gmail.com>
2022-11-02 12:44:00 +0200

    change menu styles

--- src/components/Menu/Intro.tsx
+++ src/components/Menu/Intro.tsx
@@ -4,13 +4,13 @@ import React from "react";
 const Intro = () => {
 	return (
 		<Box  className="intro">
-			<Typography variant="h3" gutterBottom>
+			{/* <Typography variant="h3" gutterBottom>
       Haluamme luoda kestäviä tuotteita ympäristöystävällisin menetelmin.
 			</Typography>
 			<Typography variant="h4" gutterBottom>
       Ajaton muotoilu, huolellisesti valikoidut värit, pitkäaikaiset suhteet asiakkaiden ja valmistajien kanssa sekä kestävän kehityksen hankkeet. 
       Tärkeää meille, tärkeää ympäristölle.
-			</Typography>
+			</Typography> */}
 		</Box>
 	);
 };
