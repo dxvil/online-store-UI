3bbd4d3fcedf13dfe22d73fc08510ae181d4c555
vol4ik europe <volikeurope@gmail.com>
2022-11-11 13:07:42 +0200

    admin page only for admin role

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,27 @@
 import React from "react";
-import { Stack, Card, CardContent, Typography } from "@mui/material";
+import { Stack, Card, CardContent, Alert, Typography } from "@mui/material";
 import { Admin } from "./Admin";
 import { useAppSelector } from "../../hooks/reduxTyped";
 
 export const Profile = () => {
 	const {user} = useAppSelector((state) => state.user);
-	
+	const customerStyles = user?.role === "customer" ? {
+		alignItems: "center",
+		justifyContent: "center"
+	} : {};
+
 	return (
 		<Stack direction="row" 
 			sx={{width: "100%", margin: "5em 0 0 0"}}
+			spacing={20}
 			justifyContent="space-between"
-			spacing={20}>
-			<Admin />
+			style={{...customerStyles}}>
+			{user?.role === "admin" && <Admin />}
+			{user?.role === "customer" &&
+			<Alert severity="info" sx={{height: "3em"}}>
+				Dear customer, here it will be your list of purchases later on.
+			</Alert>
+			}
 			<Card sx={{ 
 				maxWidth: "20%", 
 				padding: "2em", 
